8884cb31652db2a0c93ebfe1a770149cedb12514
Rinesh Ramadhin <rinesh.ramadhin@gmail.com>
2020-01-25 15:27:02 +0100

    Replaced user views with viewset, added more auth logic to serializer, added custom me view in viewset, added direct url link to obtain token viewset

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from src.user import urls as user_urls
+from src.user.views import UserViewSet
 
+router = DefaultRouter()
+
+router.register('user', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include(user_urls)),
+    path('api/user/', include(user_urls)),
+    path('api/', include(router.urls)),
 ]
