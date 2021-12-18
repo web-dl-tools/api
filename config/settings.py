68c587ba092fd358c3117911443332ab4b3a2ac7
Rinesh Ramadhin <rineshramadhin@users.noreply.github.com>
2021-12-18 19:22:21 +0100

    Added credentials update endpoint, bumped version

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from corsheaders.defaults import default_headers
 
 
 # Build information of the Web DL API
-VERSION = '2.15.0'
+VERSION = '2.16.0'
 REPOSITORY = 'https://github.com/web-dl-tools/api.git'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
