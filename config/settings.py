19e57b4ef9f1438a61296d2338433edf3194d497
Rinesh Ramadhin <RineshRamadhin@users.noreply.github.com>
2023-12-17 18:10:05 +0100

    Updated dependencies

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from corsheaders.defaults import default_headers
 
 
 # Build information of the Web DL API
-VERSION = '3.2.3'
+VERSION = '3.3.0'
 REPOSITORY = 'https://github.com/web-dl-tools/api.git'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
